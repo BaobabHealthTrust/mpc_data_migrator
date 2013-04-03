6072e9ba4ed2a04b621e87a1f0bcc79fe3f2ed05
blessings <blessings.mhango@gmail.com>
2013-04-03 13:06:34 +0200

    removed irrelevant code

--- script/migration_analysis.rb
+++ script/migration_analysis.rb
@@ -1,18 +1,12 @@
-LogBartMatCommonIds = Logger.new(Rails.root.join("log","bart_mat_common_ids.txt"))
-LogBartAncCommonIds =  Logger.new(Rails.root.join("log","bart_anc_common_ids.txt"))
-LogBartMatDiffIds = Logger.new(Rails.root.join("log","bart_mat_diff_ids.txt"))
-LogBartAncDiffIds = Logger.new(Rails.root.join("log","bart_anc_diff_ids.txt"))
 LogBartOnlyIds = Logger.new(Rails.root.join("log","bart_only_ids.txt"))
-LogMatOnlyIds = Logger.new(Rails.root.join("log","mat_only_ids.txt"))
-LogAncOnlyIds = Logger.new(Rails.root.join("log","anc_only_ids.txt"))
 LogTimeandCounts = Logger.new(Rails.root.join("log","time_and_counts.txt"))
 
 # Select all people from model that are females and count them
 def get_people(personmodel,name)
-  log_progress("Searching for #{name} female patients from :#{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
-  people = personmodel.where(:voided => 0,:gender => "F")
-  log_progress("Finished searching for #{name} female patients at:#{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
-  log_progress("Found ##{personmodel.count} female patients in #{name}",true)
+  log_progress("Searching for #{name} patients from :#{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
+  people = personmodel.where(:voided => 0)
+  log_progress("Finished searching for #{name} patients at:#{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
+  log_progress("Found ##{personmodel.count} patients in #{name}",true)
   return people
 end
 
@@ -32,73 +26,18 @@ def check_demographics
   bart_demographics =  build_demographics(get_people(Bart2Person,"BART 2.0"))
   log_progress("Finished buiding BART demographics at: #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
   log_progress("There are : ##{bart_demographics.count} patient demographics in BART2",true)
-  mat_demographics = build_demographics(get_people(MatPerson,"Martenity"))
-  log_progress("Finished buiding MAT demographics at: #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
-  log_progress("There are : ##{mat_demographics.count} patient demographics in MAT",true)
-  anc_demographics = build_demographics(get_people(AncPerson,"ANC"))
-  log_progress("Finished buiding ANC demographics at: #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
-  log_progress("There are : ##{anc_demographics.count} patient demographics in ANC",true)
-
-
-  log_progress("Searching MAT demographics at: #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
-
-  common_ids = []
-  diff_ids = []
+  
   bart_only_ids = []
   bart_demographics.each do|key,value|
     next if key.blank?
-    unless mat_demographics[key].blank?
-      if mat_demographics[key] == value
-        common_ids << key
-        log_progress("common national patient identifier >>> #{key}")
-        LogBartMatCommonIds.info key.to_s
-      else
-        diff_ids << key
-        log_progress("found in maternity but for a different person >> #{key}")
-        LogBartMatDiffIds.info key.to_s
-      end
-    else
       bart_only_ids << key
-      log_progress("not found in maternity > #{key}")
+      log_progress("found in  > #{key}")
       LogBartOnlyIds.info key.to_s  
     end
-  end
 
-
-
-  log_progress("Searching ANC demographics at: #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
-
-  anc_bart_common_ids = []
-  anc_diff_ids = []
-  bart_only_ids_anc = []
-  bart_demographics.each do|key,value|
-    next if key.blank?
-    unless anc_demographics[key].blank?
-      if anc_demographics[key] == value
-        anc_bart_common_ids << key
-        log_progress("common national patient identifier >>> #{key}")
-        LogBartAncCommonIds.info key.to_s
-      else
-        anc_diff_ids << key
-        log_progress("found in anc but for a different person >> #{key}")
-        LogBartAncDiffIds.info key.to_s
-      end
-    else
-      bart_only_ids_anc << key
-      log_progress("not found in anc > #{key}")
-      LogAncOnlyIds.info key.to_s
-    end
-  end
-
-  log_progress("##{common_ids.count} national ids found both in BART 2.0 and Maternity for the same patients",true)
-  log_progress("##{diff_ids.count} national ids found both in BART 2.0 and Maternity but for different patients",true)
   log_progress("##{bart_only_ids.count} national ids found in BART 2.0  only and not in Maternity",true)
-  
-  log_progress("##{anc_bart_common_ids.count} national ids found both in BART 2.0 and ANC for the same patients",true)
-  log_progress("##{anc_diff_ids.count} national ids found both in BART 2.0 and ANC but for different patients",true)
-  log_progress("##{bart_only_ids_anc.count} national ids found in BART 2.0  only and not in ANC",true)
-
   log_progress("Finished checking demographics at: #{Time.now().strftime('%Y-%m-%d %H:%M:%S')}",true)
+
 end
 # Get patients with identifiers only
 def build_demographics(people)
