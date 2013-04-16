b63819675f40b1bc1020a219078591790afaeeca
blessings <blessings.mhango@gmail.com>
2013-04-16 11:12:31 +0200

    person name filtering

--- app/models/person_name_code.rb
+++ app/models/person_name_code.rb
@@ -5,17 +5,27 @@ class PersonNameCode < ActiveRecord::Base
 
   def self.create_name_code(person)
     found = self.find_by_person_id(person.id)
-    return if person.given_name.blank?
-    return if person.family_name.blank?
-    return unless person.given_name.match(/null/).blank?
-    return unless person.family_name.match(/null/).blank?
+    
     return unless person.given_name.match(/[0-9]/).blank?
     return unless person.family_name.match(/[0-9]/).blank?
 
+    return if person.given_name.blank?
+    return if person.family_name.blank?
+
+    unless person.family_name.match(/[[:punct:]]/).blank?
+      return if person.family_name.match(/A'z/).blank?
+      return if person.family_name.strip.length == 1
+    end
+
+    unless person.given_name.match(/[[:punct:]]/).blank?
+      return if person.given_name.match(/A'z/).blank?
+      return if person.given_name.strip.length == 1
+    end
+
     if found.blank?
       self.create(:person_id => person.id,
-                :given_name_code => person.given_name.soundex,
-                :family_name_code => person.family_name.soundex)
+                  :given_name_code => person.given_name.soundex,
+                  :family_name_code => person.family_name.soundex)
     else
       found.given_name_code = person.given_name.soundex
       found.family_name_code = person.family_name.soundex
@@ -25,14 +35,35 @@ class PersonNameCode < ActiveRecord::Base
 
   def self.rebuild_person_name_codes
     PersonNameCode.delete_all
-    people = Person.find(:all)
-    people.each {|person|
-      PersonNameCode.create(
+    self.connection.execute('ALTER TABLE person_name_codes AUTO_INCREMENT = 1')
+    people = Person.all
+    people.each do |person|
+      
+      next if person.given_name.blank?
+      next if person.family_name.blank?
+
+      next unless person.given_name.match(/[0-9]/).blank?
+      next unless person.family_name.match(/[0-9]/).blank?
+
+      unless person.family_name.match(/[[:punct:]]/).blank?
+        next if person.family_name.match(/A'z/).blank?
+        next if person.family_name.strip.length == 1
+      end
+
+      unless person.given_name.match(/[[:punct:]]/).blank?
+        next if person.given_name.match(/A'z/).blank?
+        next if person.given_name.strip.length == 1
+      end
+      
+      p = PersonNameCode.create(
         :person_id => person.id,
-        :given_name_code => (person.given_name || '').soundex,
-        :family_name_code => (person.family_name || '').soundex
+        :given_name_code => person.given_name.soundex,
+        :family_name_code => person.family_name.soundex
       )
-    }
+
+     puts p.inspect
+     
+    end
   end
   
 end
