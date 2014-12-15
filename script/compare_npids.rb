6375f76f5bcf32ac66fefa307053bb119d293498
blessings <blessings.mhango@gmail.com>
2014-12-15 16:51:15 +0200

    finished compare ids script

--- script/compare_npids.rb
+++ script/compare_npids.rb
@@ -1,2 +1,15 @@
 site_code = SITE_CONFIG["site_id"]
 master_ids = MasterNationalPatientIdentifier.where(:pulled => true, :assigner_site_id => site_code)
+master_ids.each do |master_id|
+    national_id = NationalPatientIdentifier.find_by_value(master_id.value) rescue nil
+    if national_id.blank?
+        npid = NationaPatientIdentifier.new
+        npid.value = master_id.value
+        npid.assigner_site_id = master_id.assigner_site_id
+        npid.assigner_at = Date.today
+        npid.save
+        puts "Created npid"
+    else
+        puts "Npid already found"
+    end
+end
